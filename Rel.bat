ce7deb7e91f56943d26e71dbf26ad322df293dc7
GeoffreyAA <78430346+GeoffreyAA@users.noreply.github.com>
2026-04-22 16:52:49 +0200

    Added Rel.bat
    This batch file make putting together a release easier, copying the built binaries, text files, renamed what needs to be renamed, and zipping them all up!

---
+++ Rel.bat
@@ -0,0 +1,29 @@
+setlocal
+set bin_name=WM
+set dst_root=__Release__
+set dst=%dst_root%\%bin_name%
+
+if exist "%dst_root%" goto end
+if exist "%bin_name%.zip" goto end
+
+mkdir	"%dst%"
+mkdir	"%dst%\Languages"
+
+copy	"Build\x86\Release-XP\%bin_name%.exe" "%dst%"
+rename	"%dst%\%bin_name%.exe" "%bin_name%32.exe"
+
+copy	"Build\x64\Release\%bin_name%.exe" "%dst%"
+
+copy	"Licence.txt"     "%dst%"
+copy	"Log.txt"         "%dst%"
+copy	"Readme.md"       "%dst%"
+rename	"%dst%\Readme.md" "Readme.txt"
+
+copy	"Languages" "%dst%\Languages"
+
+"C:\Program Files\7-Zip\7z.exe" a "%bin_name%.zip" ".\%dst_root%\*" -mx9
+
+:end
+
+endlocal
+pause
